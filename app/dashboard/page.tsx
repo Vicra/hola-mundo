b3b47009c3c6aca6a71eb972b9001effeabeaab1
Victor Ramirez <Vicramirez_10@hotmail.com>
2026-07-29 12:10:07 -0600

    Layout, header and footer

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,12 +1,10 @@
-import { Button } from "@mui/material";
-import Image from "next/image";
+import { Typography } from "@mui/material";
 
-export default function Home() {
+export default function Dashboard() {
     return (
         <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
             <main className="flex flex-1 w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-                Hola next.js
-                <Button variant="outlined">Contained</Button>
+                <Typography variant="h4">Dashboard</Typography>
             </main>
         </div>
     );
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,12 @@
+"use client";
+
 import { AppRouterCacheProvider } from "@mui/material-nextjs/v15-appRouter";
 import { Roboto } from "next/font/google";
 import { ThemeProvider } from "@mui/material/styles";
 import theme from "./theme";
+import Header from "./components/header";
+import Footer from "./components/footer";
+import { usePathname } from "next/navigation";
 
 const roboto = Roboto({
     weight: ["300", "400", "500", "700"],
@@ -12,11 +17,20 @@ const roboto = Roboto({
 
 export default function RootLayout(props: { children: React.ReactNode }) {
     const { children } = props;
+    const pathname = usePathname();
+
+    const exceptionRoutes = ["/login", "/register"];
     return (
         <html lang="en" className={roboto.variable}>
             <body>
                 <AppRouterCacheProvider>
-                    <ThemeProvider theme={theme}>{children}</ThemeProvider>
+                    <ThemeProvider theme={theme}>
+                        {/* header */}
+                        {!exceptionRoutes.includes(pathname) && <Header />}
+                        {children}
+                        {/* footer */}
+                        {!exceptionRoutes.includes(pathname) && <Footer />}
+                    </ThemeProvider>
                 </AppRouterCacheProvider>
             </body>
         </html>
